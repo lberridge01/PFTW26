af3f60c08197067f1988d4287299db11a8166fbf
lberridge01 <166887048+lberridge01@users.noreply.github.com>
2026-01-29 20:02:16 -0600

    Add files via upload

---
+++ 2.1/main.js
@@ -0,0 +1,4 @@
+let myMovies = ["The Notebook", "The Devil Wears Prada", "10 Things I Hate About You", "Superbad", "Wedding Crashers"];
+const userInput = window.prompt("Add your favorite Movie");
+myMovies.push(userInput);
+console.log(myMovies.toString());

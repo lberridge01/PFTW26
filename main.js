5c1469a1f0f3c8fc97d1fc4e454da8306e9bc46d
lberridge01 <166887048+lberridge01@users.noreply.github.com>
2026-02-01 20:58:41 -0600

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-let myMovies = ["The Notebook", "The Devil Wears Prada", "10 Things I Hate About You", "Superbad", "Wedding Crashers"];
-const userInput = window.prompt("Add your favorite Movie");
-myMovies.push(userInput);
-console.log(myMovies.toString());
